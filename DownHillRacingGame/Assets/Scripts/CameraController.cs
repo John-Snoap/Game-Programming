c6be4bda42ff32551e7186d200b52890e4aaf5c3
joshua-mullen <joshua.mullen@eagles.oc.edu>
2014-02-13 03:55:22 -0600

    added downhill racing game

---
+++ DownHillRacingGame/Assets/Scripts/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+	// public data
+	public GameObject player;
+	// end public data
+
+	// private data
+	private Vector3 offset;
+	// end private data
+
+	// private methods
+	private void Start() // Use this for initialization
+	{
+		offset = transform.position - player.transform.position;
+	} // end private method Start
+
+	private void LateUpdate()
+	{
+		transform.position = player.transform.position + offset;
+	} // end private method LateUpdate
+	// end private methods
+} // end public class CameraController
---
+++ DownHillRacingGame/Assets/Scripts/FinishLine.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class FinishLine : MonoBehaviour
+{
+	// public data
+	public GUIText playerTime;
+	public GUIText finishingTime;
+	public GUIText startCountDown;
+	// end public data
+
+	// private data
+	private bool gameWon;
+	private GameObject[] gates;
+	private float countDown;
+	private float timeStart;
+	//private bool gameWon;
+	// end private data
+
+	// Use this for initialization
+	void Start ()
+	{
+		gameWon = false;
+		gates =  GameObject.FindGameObjectsWithTag("GateText");
+		
+		// Counter that controls starting countdown
+		countDown = 3.0f;
+		
+		timeStart = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if(countDown > 0)
+		{
+			startCountDown.text = "" + (int)countDown;
+			countDown -= Time.deltaTime;
+		}
+		else
+		{
+			startCountDown.text = "";
+		}
+
+		// Keep updating playerTime until they go through
+		// all the gates and they hit the bottom wall.
+		if(!gameWon && countDown < 0)
+		{
+			setCountText();
+		}
+	}
+
+	// Method for detecting collisions with the gate entrances
+	void OnTriggerEnter(Collider other)
+	{
+		// When player collides with gameObject with
+		// the tag "Gate" will then deativate the particles
+		// and add one to cubeCount.
+//		if (other.gameObject.tag == "Player")
+//		{
+//			SetGateColorText();
+//			this.gameObject.SetActive(false);
+//			gatesClearedCount = gatesClearedCount + 1;
+//		}
+
+
+		if(other.gameObject.tag == "Player")
+		{
+			foreach(GameObject gate in gates)
+			{
+				if(gate.guiText.color.Equals(Color.green))
+				{
+					gameWon = true;
+				} // end if
+				else
+				{
+					gameWon = false;
+					break; // this gets us out of the foreach loop
+				} // end else
+			} // end foreach loop
+
+			if(gameWon)
+			{
+				finishingTime.text = "Finishing " + playerTime.text;
+			} // end if
+		} // end if
+	} // end private method OnTriggerEnter
+
+	private void setCountText()
+	{
+		timeStart += Time.deltaTime;
+		playerTime.text = "Time:  " + timeStart;
+	} // end private method setCountText
+}
